746c9fbf2ab89c61731d9eb03998b342e9609653
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-13 18:18:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        handleToDo(event.target.newtask.value)
+        form.reset()
+    })
+
+})
+
+function handleToDo(todo) {
+
+    let task = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', (event) =>
+        event.target.parentNode.remove()
+
+    )
+
+    btn.textContent = "  X"
+
+    task.textContent = `   ${todo}`
+
+    task.appendChild(btn)
+
+    document.querySelector('#tasks').appendChild(task)
+
+}
